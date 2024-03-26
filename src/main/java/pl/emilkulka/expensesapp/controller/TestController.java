1f3d8990de65cbbdfa3060cf91c3cdc7ebe20260
EmilKulka <226310@student.uek.krakow.pl>
2024-03-26 10:32:57 +0100

    Refactor

--- src/main/java/pl/emilkulka/expensesapp/controller/TestController.java
+++ src/main/java/pl/emilkulka/expensesapp/controller/TestController.java
@@ -1,32 +1,30 @@
 package pl.emilkulka.expensesapp.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import pl.emilkulka.expensesapp.model.Expense;
 import pl.emilkulka.expensesapp.repository.ExpenseRepository;
 
-import java.util.List;
-
-
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/delete")
+public class TestController {
 
     private final ExpenseRepository expenseRepository;
-
     @Autowired
-    public HomeController(ExpenseRepository expenseRepository) {
+    public TestController(ExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository;
     }
 
-    @GetMapping()
-    private String home(Model model) {
-        model.addAttribute("expenses", expenseRepository.findAll());
-        return "home";
+    @GetMapping
+    private String deleteExpense() {
+        return "test";
+    }
+
+    @PostMapping
+    private String deleteById(Long id) {
+        expenseRepository.deleteById(id);
+        return "redirect:/";
     }
 }
