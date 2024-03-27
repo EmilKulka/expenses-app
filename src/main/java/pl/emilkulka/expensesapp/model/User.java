55daa0d9e6b9407bf35700b0542c903c2929d411
EmilKulka <226310@student.uek.krakow.pl>
2024-03-27 16:09:12 +0100

    Created model for User

--- src/main/java/pl/emilkulka/expensesapp/model/User.java
+++ src/main/java/pl/emilkulka/expensesapp/model/User.java
@@ -1,2 +1,34 @@
-package pl.emilkulka.expensesapp.model;public class User {
+package pl.emilkulka.expensesapp.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.emilkulka.expensesapp.exception.user.EmailFormatException;
+import pl.emilkulka.expensesapp.validator.user.EmailValidator;
+
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+
+    @Transient
+    private final EmailValidator emailValidator = new EmailValidator();
+
+    @Getter
+    private String email;
+
+    public void setEmail(String email) {
+        if (!emailValidator.isValid(email)) {
+            throw new EmailFormatException();
+        }
+        this.email = email;
+    }
 }
+
