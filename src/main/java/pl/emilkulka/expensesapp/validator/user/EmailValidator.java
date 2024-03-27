23987887a3385046af1cb6d3aac992bd31e6c303
EmilKulka <226310@student.uek.krakow.pl>
2024-03-27 16:09:52 +0100

    Created validator for User email field

--- src/main/java/pl/emilkulka/expensesapp/validator/user/EmailValidator.java
+++ src/main/java/pl/emilkulka/expensesapp/validator/user/EmailValidator.java
@@ -1,5 +1,8 @@
-package pl.emilkulka.expensesapp.validator;
-public class EmailValidator implements FieldValidator<String>{
+package pl.emilkulka.expensesapp.validator.user;
+
+import pl.emilkulka.expensesapp.validator.FieldValidator;
+
+public class EmailValidator implements FieldValidator<String> {
     public boolean isValid(String email) {
         return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
     }
