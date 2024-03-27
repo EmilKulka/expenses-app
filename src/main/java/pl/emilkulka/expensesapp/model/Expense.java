4d2f1068484713b3137fb2e7b209316b5cc02c61
EmilKulka <226310@student.uek.krakow.pl>
2024-03-27 15:58:24 +0100

    Implemented validators to Expense class fields

--- src/main/java/pl/emilkulka/expensesapp/model/Expense.java
+++ src/main/java/pl/emilkulka/expensesapp/model/Expense.java
@@ -1,12 +1,17 @@
 package pl.emilkulka.expensesapp.model;
 
 import jakarta.persistence.*;
-import pl.emilkulka.expensesapp.exception.DateFromFutureException;
-import pl.emilkulka.expensesapp.exception.DescriptionLimitException;
-import pl.emilkulka.expensesapp.exception.NegativePriceException;
+import pl.emilkulka.expensesapp.exception.expense.DateFromFutureException;
+import pl.emilkulka.expensesapp.exception.expense.DescriptionLimitException;
+import pl.emilkulka.expensesapp.exception.expense.InvalidTypeException;
+import pl.emilkulka.expensesapp.exception.expense.NegativePriceException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.emilkulka.expensesapp.validator.expense.DateValidator;
+import pl.emilkulka.expensesapp.validator.expense.DescriptionValidator;
+import pl.emilkulka.expensesapp.validator.expense.ExpenseTypeValidator;
+import pl.emilkulka.expensesapp.validator.expense.PriceValidator;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -26,51 +31,61 @@ public class Expense {
     private BigDecimal price;
     private LocalDate date;
 
+    @Transient
+    private final ExpenseTypeValidator expenseTypeValidator = new ExpenseTypeValidator();
+    @Transient
+    private final DescriptionValidator descriptionValidator = new DescriptionValidator();
+    @Transient
+    private final PriceValidator priceValidator = new PriceValidator();
+    @Transient
+    private final DateValidator dateValidator = new DateValidator();
+
     public Expense(ExpenseType type, String description, BigDecimal price, LocalDate date) {
+
+        if (expenseTypeValidator.isValid(type)) {
+            throw new InvalidTypeException();
+        }
         this.type = type;
-        if (isDescriptionInvalid(description)) {
+
+        if (descriptionValidator.isValid(description)) {
             throw new DescriptionLimitException();
         }
         this.description = description;
-        if (isPriceNegative(price)) {
+
+        if (priceValidator.isValid(price)) {
             throw new NegativePriceException();
         }
         this.price = price;
-        if(isDateFromFuture(date)) {
+
+        if (dateValidator.isValid(date)) {
             throw new DateFromFutureException();
         }
         this.date = date;
     }
 
-    public boolean isDescriptionInvalid(String description) {
-        return description.length() > 100;
+    public void setType(ExpenseType type) {
+        if (expenseTypeValidator.isValid(type)) {
+            throw new InvalidTypeException();
+        }
+        this.type = type;
     }
 
     public void setDescription(String description) {
-        if (isDescriptionInvalid(description)) {
+        if (descriptionValidator.isValid(description)) {
             throw new DescriptionLimitException();
         }
         this.description = description;
     }
 
-    public boolean isPriceNegative(BigDecimal price) {
-        return price.compareTo(BigDecimal.ZERO) < 0;
-    }
-
     public void setPrice(BigDecimal price) {
-        if (isPriceNegative(price)) {
+        if (priceValidator.isValid(price)) {
             throw new NegativePriceException();
         }
         this.price = price;
     }
 
-    public boolean isDateFromFuture(LocalDate date) {
-        LocalDate localDate = LocalDate.now();
-        return date.isAfter(localDate);
-    }
-
     public void setDate(LocalDate date) {
-        if(isDateFromFuture(date)) {
+        if (dateValidator.isValid(date)) {
             throw new DateFromFutureException();
         }
         this.date = date;
