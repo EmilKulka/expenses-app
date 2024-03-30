d0ae6f2fa308ac2c2b1ab7780ba5813318ed5008
EmilKulka <226310@student.uek.krakow.pl>
2024-03-30 19:13:19 +0100

    Modified controller to validate field input

--- src/main/java/pl/emilkulka/expensesapp/controller/ExpenseAddController.java
+++ src/main/java/pl/emilkulka/expensesapp/controller/ExpenseAddController.java
@@ -1,29 +1,46 @@
 package pl.emilkulka.expensesapp.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.emilkulka.expensesapp.exception.expense.DateFromFutureException;
+import pl.emilkulka.expensesapp.exception.expense.DescriptionLimitException;
+import pl.emilkulka.expensesapp.exception.expense.InvalidTypeException;
+import pl.emilkulka.expensesapp.exception.expense.NegativePriceException;
 import pl.emilkulka.expensesapp.model.Expense;
 import pl.emilkulka.expensesapp.repository.ExpenseRepository;
+import pl.emilkulka.expensesapp.service.ExpenseService;
 
 @Controller
 @RequestMapping("/expense-add")
 public class ExpenseAddController {
     private final ExpenseRepository expenseRepository;
+    private final ExpenseService expenseService;
+
     @Autowired
-    public ExpenseAddController(ExpenseRepository expenseRepository) {
+    public ExpenseAddController(ExpenseRepository expenseRepository, ExpenseService expenseService) {
         this.expenseRepository = expenseRepository;
+        this.expenseService = expenseService;
     }
 
     @GetMapping()
-    private String expenseAdd() {
+    private String expenseAdd(Model model) {
+        model.addAttribute("expense", new Expense());
         return "expense-add";
     }
+
     @PostMapping
-    private String saveExpense(Expense expense) {
-        expenseRepository.save(expense);
+    private String saveExpense(@Valid Expense expense, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "expense-add";
+        }
+        expenseService.saveExpense(expense);
         return "redirect:/expense-add";
     }
 }
