5839c2b74cc08c022fe1b2ccabec489be0ff52a7
EmilKulka <226310@student.uek.krakow.pl>
2024-03-31 20:04:02 +0200

    Added functionality to control expense editing

--- src/main/java/pl/emilkulka/expensesapp/controller/ExpenseListController.java
+++ src/main/java/pl/emilkulka/expensesapp/controller/ExpenseListController.java
@@ -1,21 +1,25 @@
 package pl.emilkulka.expensesapp.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.emilkulka.expensesapp.model.Expense;
 import pl.emilkulka.expensesapp.repository.ExpenseRepository;
+import pl.emilkulka.expensesapp.service.ExpenseService;
 
 @Controller
 @RequestMapping("/expense-list")
 public class ExpenseListController {
     private final ExpenseRepository expenseRepository;
+    private final ExpenseService expenseService;
 
     @Autowired
-    public ExpenseListController(ExpenseRepository expenseRepository) {
+    public ExpenseListController(ExpenseRepository expenseRepository, ExpenseService expenseService) {
         this.expenseRepository = expenseRepository;
+        this.expenseService = expenseService;
     }
 
     @GetMapping()
@@ -26,7 +30,30 @@ public class ExpenseListController {
 
     @PostMapping
     private String deleteById(Long id) {
-        expenseRepository.deleteById(id);
+        expenseService.deleteById(id);
+        return "redirect:/expense-list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editExpense(@PathVariable Long id, Model model) {
+        model.addAttribute("expense", expenseService.getExpenseById(id));
+        return "expense-edit";
+    }
+
+    @PostMapping("/{id}")
+    public String updateExpense(@PathVariable Long id, @ModelAttribute("expense") @Valid Expense expense, BindingResult bindingResult, Model model) {
+        Expense expenseToBeEdited = expenseService.getExpenseById(id);
+
+        expenseToBeEdited.setId(id);
+        expenseToBeEdited.setType(expense.getType());
+        expenseToBeEdited.setDescription(expense.getDescription());
+        expenseToBeEdited.setPrice(expense.getPrice());
+        expenseToBeEdited.setDate(expense.getDate());
+
+        if(bindingResult.hasErrors()) {
+            return "expense-edit";
+        }
+        expenseService.updateExpense(expenseToBeEdited);
         return "redirect:/expense-list";
     }
 }
